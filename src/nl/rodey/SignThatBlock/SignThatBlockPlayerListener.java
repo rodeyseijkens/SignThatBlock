ed41f2ebf1b96d24cb5293223518178143f8267d
Rodey Seijkens <me@rodey.nl>
2012-04-20 23:36:05 +0200

    Fixed Text on signs, Added Permission "signthatblock.attach"

--- src/nl/rodey/SignThatBlock/SignThatBlockPlayerListener.java
+++ src/nl/rodey/SignThatBlock/SignThatBlockPlayerListener.java
@@ -5,6 +5,8 @@ import java.util.logging.Logger;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
+import org.bukkit.block.Sign;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
@@ -24,6 +26,7 @@ public class SignThatBlockPlayerListener implements Listener
 	@EventHandler
 	public void onPlayerInteract(PlayerInteractEvent event) 
 	{
+		Player player = event.getPlayer();
 		Block block = event.getClickedBlock();
         if (block == null) return; 
         
@@ -41,49 +44,135 @@ public class SignThatBlockPlayerListener implements Listener
 			{ 
 				log.info("["+plugin.getDescription().getName()+"] Block is Right Clicked!");
 			}
-
-			if(plugin.debug)
-			{ 
-				log.info("["+plugin.getDescription().getName()+"] Player is allowed");
-			}
 			
 			if(block.getType().equals(Material.SIGN_POST))
 			{
-				if(plugin.debug)
-				{ 
-					log.info("["+plugin.getDescription().getName()+"] Clicked block is sign post");
-				}
-				
-				for ( String defBlock : definedBlocksArray)
+				if(player.hasPermission("signthatblock.attach"))
 				{
 					if(plugin.debug)
 					{ 
-						log.info("["+plugin.getDescription().getName()+"] Defined Block: "+defBlock);
+						log.info("["+plugin.getDescription().getName()+"] Clicked block is sign post");
 					}
 					
-					if (block.getRelative(BlockFace.NORTH).getTypeId() == Integer.parseInt(defBlock)) 
+					for ( String defBlock : definedBlocksArray)
 					{
-						block.setTypeId(68); //Change to Wall Sign
-						block.setData((byte) 0x5); // Set Direction
-						return;
-					}
-					else if (block.getRelative(BlockFace.EAST).getTypeId() == Integer.parseInt(defBlock)) 
-					{
-						block.setTypeId(68); //Change to Wall Sign
-						block.setData((byte) 0x3); // Set Direction
-						return;
-					}
-					else if (block.getRelative(BlockFace.SOUTH).getTypeId() == Integer.parseInt(defBlock)) 
-					{
-						block.setTypeId(68); //Change to Wall Sign
-						block.setData((byte) 0x4); // Set Direction
-						return;
-					}
-					else if (block.getRelative(BlockFace.WEST).getTypeId() == Integer.parseInt(defBlock)) 
-					{
-						block.setTypeId(68); //Change to Wall Sign
-						block.setData((byte) 0x2); // Set Direction
-						return;
+						if(plugin.debug)
+						{ 
+							log.info("["+plugin.getDescription().getName()+"] Defined Block: "+defBlock);
+						}
+						
+						if (block.getRelative(BlockFace.NORTH).getTypeId() == Integer.parseInt(defBlock)) 
+						{
+		
+							Sign signBlock = (Sign) block.getState();
+							String[] signText = signBlock.getLines(); //Get Lines
+				            
+							block.setTypeId(68); // Change to Wall Sign
+							block.setData((byte) 0x5); // Set Direction
+		
+							signBlock = (Sign) block.getState(); // Get New State
+							
+							int i = 0; // Set Line Count
+							for ( String lineText : signText)
+							{
+								signBlock.setLine(i, lineText); // Write Line
+		
+								if(plugin.debug)
+								{ 
+									log.info("["+plugin.getDescription().getName()+"] Line "+i+": "+lineText);
+								}
+								
+								i++; // Update Line Count
+								
+								signBlock.update(); // Save Sign
+							}
+							
+							return;
+						}
+						else if (block.getRelative(BlockFace.EAST).getTypeId() == Integer.parseInt(defBlock)) 
+						{
+		
+							Sign signBlock = (Sign) block.getState();
+							String[] signText = signBlock.getLines(); //Get Lines
+				            
+							block.setTypeId(68); // Change to Wall Sign
+							block.setData((byte) 0x3); // Set Direction
+		
+							signBlock = (Sign) block.getState(); // Get New State
+							
+							int i = 0; // Set Line Count
+							for ( String lineText : signText)
+							{
+								signBlock.setLine(i, lineText); // Write Line
+		
+								if(plugin.debug)
+								{ 
+									log.info("["+plugin.getDescription().getName()+"] Line "+i+": "+lineText);
+								}
+								
+								i++; // Update Line Count
+								
+								signBlock.update(); // Save Sign
+							}
+							
+							return;
+						}
+						else if (block.getRelative(BlockFace.SOUTH).getTypeId() == Integer.parseInt(defBlock)) 
+						{
+		
+							Sign signBlock = (Sign) block.getState();
+							String[] signText = signBlock.getLines(); //Get Lines
+				            
+							block.setTypeId(68); // Change to Wall Sign
+							block.setData((byte) 0x4); // Set Direction
+		
+							signBlock = (Sign) block.getState(); // Get New State
+							
+							int i = 0; // Set Line Count
+							for ( String lineText : signText)
+							{
+								signBlock.setLine(i, lineText); // Write Line
+		
+								if(plugin.debug)
+								{ 
+									log.info("["+plugin.getDescription().getName()+"] Line "+i+": "+lineText);
+								}
+								
+								i++; // Update Line Count
+								
+								signBlock.update(); // Save Sign
+							}
+							
+							return;
+						}
+						else if (block.getRelative(BlockFace.WEST).getTypeId() == Integer.parseInt(defBlock)) 
+						{
+		
+							Sign signBlock = (Sign) block.getState();
+							String[] signText = signBlock.getLines(); //Get Lines
+				            
+							block.setTypeId(68); // Change to Wall Sign
+							block.setData((byte) 0x2); // Set Direction
+		
+							signBlock = (Sign) block.getState(); // Get New State
+							
+							int i = 0; // Set Line Count
+							for ( String lineText : signText)
+							{
+								signBlock.setLine(i, lineText); // Write Line
+		
+								if(plugin.debug)
+								{ 
+									log.info("["+plugin.getDescription().getName()+"] Line "+i+": "+lineText);
+								}
+								
+								i++; // Update Line Count
+								
+								signBlock.update(); // Save Sign
+							}
+							
+							return;
+						}
 					}
 				}
 			}
